d1d2b4b147096ae2685e096690e4a4aea5054327
lalinsan <eduardoaguirre@Eduardos-iMac.local>
2016-06-12 11:15:29 -0700

    Three  Integer Variables
    Declare three integer variables (a,b,c) in a single declaration and
    simultaneously initialize b to 4

---
+++ src/Declare3Integers.java
@@ -0,0 +1,20 @@
+/*
+ * Student: Eduardo Aguirre
+ * Session 1
+ * Assignment 2
+ * Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4.
+ */
+
+
+public class Declare3Integers {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		int a, b=4, c;
+
+		System.out.println("=======================");
+		System.out.println("Integer b : " +b);
+		System.out.println("=======================");
+	}
+
+}
